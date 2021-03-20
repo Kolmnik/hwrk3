c08edaf70582586c3e6a5455fe135b43114201b0
nikita <mrxloack@yandex.ru>
2021-03-20 14:12:34 +0300

    innital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {v1} from 'uuid';
 import './App.css';
 
+type userType = {
+    id: string
+    name: string
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const addUser = (name: string) => {
+        const newUser: userType = {
+            id: v1(),
+            name: name
+        }
+        const newAddUser = [newUser, ...user]
+        setUser(newAddUser)
+    }
+    let [user, setUser] = useState<Array<userType>>([])
+
+    const [name, setName] = useState<string>('')
+    const changeName = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+    }
+    const changeNameEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            addName()
+        }
+    }
+    const addName = () => {
+        addUser(name)
+        setName(' ')
+        alert('Hi ' + name)
+    }
+
+    return (
+        <div className="App">
+            <h1>Homework 3</h1>
+            <div className={'addInput'}>
+                <input value={name} type="text" onChange={changeName} onKeyPress={changeNameEnter}/>
+                <button onClick={addName}>Add</button>
+            </div>
+            Количество юзеров: {user.length}
+            <div>
+                {user.map((u) => {
+                    return <div> {u.name}</div>
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
